e1126370235e13fd2f278ac45f52701d86cd5923
Andrii <kulchaka@gmail.com>
2021-02-13 16:54:53 +0100

    move to classes

--- src/js/model.js
+++ src/js/model.js
@@ -1,20 +1,31 @@
 import image from "../img/javascript-illustration.png";
+import { Block } from "./classes/blocks";
 
 export const model = [
-  {
-    type: "title",
-    value: "Contructor on Vanilla JS ",
-    options: {
-      tag: "h2",
-      styles: {
-        background: "linear-gradient(to right, #f68e43 0%, #fbda03 100%)",
-        color: "#4b4d65",
-        "text-align": "center",
-        "text-shadow": "1px 1px 2px #383114",
-        padding: "1rem",
-      },
+  new Block("title", "Contructor on Vanilla JS", {
+    tag: "h2",
+    styles: {
+      background: "linear-gradient(to right, #f68e43 0%, #fbda03 100%)",
+      color: "#4b4d65",
+      "text-align": "center",
+      "text-shadow": "1px 1px 2px #383114",
+      padding: "1rem",
     },
-  },
+  }),
+  // {
+  //   type: "title",
+  //   value: "Contructor on Vanilla JS ",
+  //   options: {
+  //     tag: "h2",
+  //     styles: {
+  //       background: "linear-gradient(to right, #f68e43 0%, #fbda03 100%)",
+  //       color: "#4b4d65",
+  //       "text-align": "center",
+  //       "text-shadow": "1px 1px 2px #383114",
+  //       padding: "1rem",
+  //     },
+  //   },
+  // },
   {
     type: "image",
     value: image,
